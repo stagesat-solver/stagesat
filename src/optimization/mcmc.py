4e96414541b0ae6f8859da53a77902cc8544a7af
yuanzhuouiuc <yzzhanghust@gmail.com>
2025-10-13 21:33:40 -0400

    refine mcmc

--- src/optimization/mcmc.py
+++ src/optimization/mcmc.py
@@ -55,14 +55,12 @@ def nth_fp_vectorized(n, x):
     s = struct.pack('<d', x)
     i = struct.unpack('<Q', s)[0]
     m = i + n
-    # m = n + struct.unpack('!i',struct.pack('!f',x))[0]
     if m < 0:
         sign_bit = 0x8000000000000000
         m = -m
     else:
         sign_bit = 0
     if m >= 0x7ff0000000000000:
-        warnings.warn("Value out of range, with n= %g,x=%g,m=%g, process=%g" % (n, x, m, mp.current_process.name))
         m = 0x7ff0000000000000
     bit_pattern = struct.pack('Q', m | sign_bit)
     return struct.unpack('d', bit_pattern)[0]
@@ -81,7 +79,6 @@ def nth_fp32_vectorized(n, x):
     else:
         sign_bit = 0
     if m >= 0x7f800000:  # Float32 infinity
-        # warnings.warn(f"Float32 value out of range, n={n}, x={x}")
         m = 0x7f800000
     bit_pattern = struct.pack('I', m | sign_bit)
     return struct.unpack('f', bit_pattern)[0]
@@ -109,37 +106,66 @@ def mcmc(args, i, stop_event):
                               minimizer_kwargs=_minimizer_kwargs, callback=callback)
         if args.showResult:
             print("result (round 1) with i = ", i, ":")
-            # print(f"{res.fun} + {scale(res.x, i)}")
             print(f"{res.fun}")
             print()
         X_star = scale(res.x, i)
-        R_star = res.fun
+        # R_star = res.fun
         if stop_event.is_set():
             break
-        if res.fun >= args.round2_threshold:
+        if res.fun >= args.round1_threshold:
+            # bad results, retry
             continue
         ########################################################################################
+        # optional round 2, do it when r1 round is bad
+        if round_num > args.round2_activate and best_R_star > args.round2_threshold:
+            if args.showTime:
+                print("[Xsat] round2_move!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            rec = foo_ulp.R(*X_star)
+            sp2 = np.array([X_star + 0]) if X_star.ndim == 0 else X_star
+            res_round2 = op.basinhopping(
+                lambda X: foo_ulp.R(*scale(X, i)),
+                sp2,
+                niter=args.round2_niter,
+                stepsize=args.round2_stepsize,
+                minimizer_kwargs=_minimizer_kwargs,
+                callback=callback
+            )
+            if stop_event.is_set():
+                break
+            if res_round2.fun < rec:
+                R_star = res_round2.fun
+                X_star = scale(tr_help(res_round2.x), i)
+            else:
+                R_star = rec
+            if R_star < best_R_star:
+                best_R_star = R_star
+                best_X_star = X_star
+                if args.showResult:
+                    print(f"//////////////////////////////R2 found a best_R_star: {best_R_star} + R1: {res.fun}")
+            if best_R_star == 0:
+                break
+        ########################################################################################
         if args.showTime:
-            print("[Xsat] round2_move!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        sp2 = np.array([X_star + 0]) if X_star.ndim == 0 else X_star
-        def obj_near2(N):
-            X2_moved = np.array([
+            print("[Xsat] round3_move!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        sp3 = np.array([X_star + 0]) if X_star.ndim == 0 else X_star
+        def obj_near3(N):
+            X3_moved = np.array([
                 nth_fp_dispatchers[j](n_val, x_base_val)
-                for j, (n_val, x_base_val) in enumerate(zip(N, sp2))
+                for j, (n_val, x_base_val) in enumerate(zip(N, sp3))
             ])
-            return foo_ulp.R(*X2_moved)
-        res_round2 = op.basinhopping(obj_near2, np.zeros(foo_ulp.dim), niter=args.round2_niter,
-                                     stepsize=args.round2_stepsize, minimizer_kwargs=_minimizer_kwargs,
+            return foo_ulp.R(*X3_moved)
+        res_round3 = op.basinhopping(obj_near3, np.zeros(foo_ulp.dim), niter=args.round3_niter,
+                                     stepsize=args.round3_stepsize, minimizer_kwargs=_minimizer_kwargs,
                                      callback=callback)
-        R_star = res_round2.fun
+        R_star = res_round3.fun
         X_star = np.array([
             nth_fp_dispatchers[j](n_val, x_base_val)
-            for j, (n_val, x_base_val) in enumerate(zip(res_round2.x, sp2))])
+            for j, (n_val, x_base_val) in enumerate(zip(res_round3.x, sp3))])
         if R_star < best_R_star:
             best_R_star = R_star
             best_X_star = X_star
             if args.showResult:
-                print(f"//////////////////////////////R2 found a best_R_star: {best_R_star} + R1: {res.fun}")
+                print(f"//////////////////////////////R3 found a best_R_star: {best_R_star} + R1: {res.fun}")
         if best_R_star == 0:
             break
     return best_X_star, best_R_star
